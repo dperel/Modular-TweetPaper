911a2ac528bb285f23b15a18dff423a9717425c1
dperel <davidperel.pdf@gmail.com>
2015-09-28 15:12:58 -0400

    Learned lesson: To access user session in modular engine, you need to add the application controller current_user helper method into the engine's application controller.

--- engines/frontpage/app/controllers/frontpage/frontpages_controller.rb
+++ engines/frontpage/app/controllers/frontpage/frontpages_controller.rb
@@ -3,7 +3,8 @@ require_dependency "frontpage/application_controller"
 module Frontpage
   class FrontpagesController < ApplicationController
     def index
+      binding.pry
     end
-    
+
   end
 end
