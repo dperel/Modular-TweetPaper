8ba3a690a5ff6ded1196835a7d14c6650de47705
dperel <davidperel.pdf@gmail.com>
2015-09-28 17:18:46 -0400

    Using a modular engine creates database issues. Instead of using one database per modular engine, I decided to make one central db and add migrations as needed, which is modular enough. This should make namespacing easier.

--- db/migrate/20150928191659_rename_frontpage_frontpages.rb
+++
@@ -1,9 +0,0 @@
-class RenameFrontpageFrontpages < ActiveRecord::Migration
-  def self.up
-     rename_table :frontpage_frontpages, :articles
-  end
-
-  def self.down
-     rename_table :articles, :frontpage_frontpages
-  end
-end
--- engines/frontpage/db/migrate/20150928194512_rename_front_page_front_pages.rb
+++
@@ -1,9 +0,0 @@
-class RenameFrontPageFrontPages < ActiveRecord::Migration
-  def self.up
-     rename_table :frontpage_frontpages, :articles
-  end
-
-  def self.down
-     rename_table :articles, :frontpage_frontpages
-  end
-end
